354d7b613cf26d26bc65e92ecd7d6a76c2994406
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-04-05 12:38:13 +0530

    3

---
+++ 3.js
@@ -0,0 +1,11 @@
+// var a = 45;
+// var a = "p";
+let b = "Kunal";
+const author = "Kunal Adwani"; // const is used to declare a constant variable
+let c = null; // null is used to declare a variable with no value
+let d = undefined;  // undefined is used to declare a variable with no value
+{ // block scope
+    let b = "Adwani" // let is used to declare a variable with block scope
+    console.log(b)  // Adwani
+}
+console.log(b) // KunalZ
