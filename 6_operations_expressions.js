e88f5fae6440df98cf3141ea8092936197b92d99
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-04-05 15:07:59 +0530

    6

---
+++ 6_operations_expressions.js
@@ -0,0 +1,41 @@
+// OPERATORS AND EXPRESSIONS IN JAVASCRIPT
+let a = 45;
+let b = 5;
+console.log("a + b =",a+b); // 50
+console.log("a - b",a-b); // 40
+console.log("a * b",a*b); // 225
+console.log("a / b",a/b); // 9
+console.log("a % b",a%b); // 0
+console.log("a ** b",a**b); // 3.7592186044416e+9
+console.log("++a",++a); // 46
+
+console.log("a++",a++); // 46
+console.log("a",a); // 47
+
+console.log("--a",--a); // 46
+console.log("a--",a--); // 46
+console.log("a",a); // 45
+
+console.log("a += b",a+=b); // 50
+console.log("a -= b",a-=b); // 45
+console.log("a *= b",a*=b); // 225
+console.log("a /= b",a/=b); // 45
+console.log("a %= b",a%=b); // 0
+console.log("a **= b",a**=b); // 0
+
+// COMPARISON OPERATORS
+console.log("a == b",a==b); // false
+console.log("a != b",a!=b); // true
+console.log("a > b",a>b); // true
+console.log("a < b",a<b); // false
+console.log("a >= b",a>=b); // true
+console.log("a <= b",a<=b); // false 
+console.log("a === b",a===b); // false comparing two values and their types
+console.log("a !== b",a!==b); // true comparing two values and their types
+
+// LOGICAL OPERATORS
+console.log("a > 0 && b > 0",a > 0 && b > 0); // true 0 and 1
+console.log("a > 0 || b > 0",a > 0 || b > 0); // true 0 or 1
+console.log("!a",!a); // false not
+
+
