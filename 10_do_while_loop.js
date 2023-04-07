6901eea56b605b818255727051fc1b7edddf41ef
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-04-07 16:53:38 +0530

    10

---
+++ 10_do_while_loop.js
@@ -0,0 +1,20 @@
+let prompt = require('prompt-sync')();
+let x = prompt("Enter a number: ");
+x = Number.parseInt(x);
+let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let first = 0;
+let last = n.length - 1;
+let mid = 0;
+while (first <= last){
+    let mid=Math.floor((first + last)/2);
+    if (x == n[mid]) {
+        console.log("Found");
+        break;
+    } 
+    else if (x > n[mid]) {
+        first = mid;
+    } 
+    else {
+        last = mid;
+    }
+}
