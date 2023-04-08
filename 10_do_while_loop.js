40af4e3cab6c7633a648c0507311173fcecc4eca
Kunal Adwani <101789307+Kunal-63@users.noreply.github.com>
2023-04-08 11:09:35 +0530

    do while

--- 10_do_while_loop.js
+++ 10_do_while_loop.js
@@ -1,20 +1,30 @@
+// let prompt = require('prompt-sync')();
+// let x = prompt("Enter a number: ");
+// x = Number.parseInt(x);
+// let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let first = 0;
+// let last = n.length - 1;
+// let mid = 0;
+// while (first <= last){
+//     let mid=Number.parseInt((first + last)/2);
+//     if (x == n[mid]) {
+//         console.log("Found");
+//         break;
+//     } 
+//     else if (x > n[mid]) {
+//         first = mid;
+//     } 
+//     else {
+//         last = mid;
+//     }
+// }
+
 let prompt = require('prompt-sync')();
 let x = prompt("Enter a number: ");
 x = Number.parseInt(x);
-let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let first = 0;
-let last = n.length - 1;
-let mid = 0;
-while (first <= last){
-    let mid=Number.parseInt((first + last)/2);
-    if (x == n[mid]) {
-        console.log("Found");
-        break;
-    } 
-    else if (x > n[mid]) {
-        first = mid;
-    } 
-    else {
-        last = mid;
-    }
-}
+let i = 0;
+do {
+    console.log(i);
+    i++;
+
+} while (i<x);
